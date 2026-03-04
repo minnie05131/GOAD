2a43c6242a3ba25b61f89477de56e4b74404671f
minnie05131 <minnie05131@outlook.com>
2026-03-04 13:14:36 -0800

    Update windows.tf

--- ad/GOAD-Mini/providers/azure/windows.tf
+++ ad/GOAD-Mini/providers/azure/windows.tf
@@ -6,5 +6,5 @@
   windows_version    = "latest" # deprecated : "2019.0.20181122"
   private_ip_address = "{{ip_range}}.10"
   password           = "8dCT-DJjgScp"
-  size               = "Standard_B2s"
+  size               = "Standard_B2als_v2"
 }
